78de2a84402bcd7451279bfb602c3384bdefbce4
crazymugg <crazymugg.gaming@gmail.com>
2025-08-25 10:27:48 -0500

    [feature] Image Carousel added

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,1 +1,9 @@
-console.log("Hello There!")
+import Splide from '@splidejs/splide';
+
+
+new Splide( '.splide', {
+        type    : 'loop',
+        autoplay: 'play',
+        perPage : 1,
+    }
+).mount();
